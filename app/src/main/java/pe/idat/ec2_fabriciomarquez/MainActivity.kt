6a3a23da545a8a46bab5b091e2a8284c86c57b13
Wilson1089 <fabriciomarquez1089@gmail.com>
2024-07-01 21:55:25 -0500

    preunta1 final2

--- app/src/main/java/pe/idat/ec2_fabriciomarquez/MainActivity.kt
+++ app/src/main/java/pe/idat/ec2_fabriciomarquez/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
@@ -67,7 +68,10 @@ fun Navegacion() {
                 Text("P2")
             }
         })
-    preg1(radios) { radios = it }
+    Box(modifier = Modifier.fillMaxWidth()){
+        preg1(radios) { radios = it }
+    }
+
 }
 
 
