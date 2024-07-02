1e1e22e3e09cb9e280df5b6a9759799323770abf
Wilson1089 <fabriciomarquez1089@gmail.com>
2024-07-01 19:42:37 -0500

    creacion

---
+++ app/src/main/java/pe/idat/ec2_fabriciomarquez/MainActivity.kt
@@ -0,0 +1,47 @@
+package pe.idat.ec2_fabriciomarquez
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pe.idat.ec2_fabriciomarquez.ui.theme.EC2_FabricioMarquezTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EC2_FabricioMarquezTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EC2_FabricioMarquezTheme {
+        Greeting("Android")
+    }
+}
